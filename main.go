7f5cd71399a651d8b23420208350d8f854acbe9b
Alex A. Evans <aevans03@byu.edu>
2026-07-21 13:45:24 -0600

    Add server-rendered blank week

--- main.go
+++ main.go
@@ -4,23 +4,65 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 )
 
+type Block struct {
+	Active bool
+	Time   string
+}
+
+type Hour struct {
+	Blocks []Block // 6 blocks to an hour
+}
+
+type Day struct {
+	DayName string
+	Hours   []Hour
+}
+
+type Week struct {
+	Days []Day
+}
+
+func blankWeek() Week {
+	dayNames := []string{"Mon", "Tues", "Wed", "Thurs", "Fri"}
+
+	// Loop through all days
+	var week []Day
+	for _, value := range dayNames {
+		var hourList []Hour
+
+		// Loop through all hours
+		for hour := 800; hour < 1800; hour += 100 {
+			var blockList []Block
+			for minutes := 0; minutes <= 50; minutes += 10 {
+				blockList = append(blockList, Block{false, strconv.Itoa(hour + minutes)})
+			}
+			hourList = append(hourList, Hour{blockList})
+		}
+		week = append(week, Day{value, hourList})
+	}
+
+	return Week{week}
+}
+
 func getHome(writer http.ResponseWriter, request *http.Request) {
+	data := blankWeek()
 
 	files := []string{
 		"./templates/base.html",
 		"./templates/schedule.html",
 	}
 
-	ts, err := template.ParseFiles(files...)
+	template, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	err = ts.ExecuteTemplate(writer, "base", nil)
+	err = template.ExecuteTemplate(writer, "base", data)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
