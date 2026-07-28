26b464f0062c3b897cb2566f62d2edb93d8525b8
Alex A. Evans <aevans03@byu.edu>
2026-07-28 14:28:30 -0600

    Proper status message display, change to view schedule mode on submit

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -54,6 +54,7 @@ func getSchedule(writer http.ResponseWriter, request *http.Request) {
 	files := []string{
 		"./templates/view-schedule.html",
 		"./templates/week-view.html",
+		"./templates/status-message.html",
 	}
 
 	template, err := template.ParseFiles(files...)
@@ -64,6 +65,12 @@ func getSchedule(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	err = template.ExecuteTemplate(writer, "view-schedule", data)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	err = template.ExecuteTemplate(writer, "status-message-clear-oob", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
@@ -80,6 +87,7 @@ func getScheduleEdit(writer http.ResponseWriter, request *http.Request) {
 	files := []string{
 		"./templates/edit-schedule.html",
 		"./templates/week-view.html",
+		"./templates/status-message.html",
 	}
 
 	template, err := template.ParseFiles(files...)
@@ -95,6 +103,11 @@ func getScheduleEdit(writer http.ResponseWriter, request *http.Request) {
 		http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
 	}
 
+	err = template.ExecuteTemplate(writer, "status-message-clear-oob", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func postScheduleSubmit(writer http.ResponseWriter, request *http.Request) {
@@ -118,30 +131,77 @@ func postScheduleSubmit(writer http.ResponseWriter, request *http.Request) {
 	// Schedule validation
 	findings = append(findings, validateSchedule(selected)...)
 
-	var data StatusMessage
-
-	if userSchedule.Approved == true {
-		data = StatusMessage{"failure", "Failure", []string{"Schedule has already been approved. Reset to submit a new one"}}
-	} else if len(findings) > 0 {
-		data = StatusMessage{"failure", "Failure", findings}
-	} else {
-		// Update/save week in server after validation
-		updateWeek(selected)
-		data = StatusMessage{"success", "Success", []string{"Schedule has been submitted for admin review"}}
-	}
-
-	template, err := template.ParseFiles("./templates/status-message.html")
+	statusTemplate, err := template.ParseFiles("./templates/status-message.html")
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	err = template.ExecuteTemplate(writer, "status-message", data)
+	if userSchedule.Approved == true {
+		data := StatusMessage{
+			"failure",
+			"Failure",
+			[]string{"Schedule has already been approved. Reset to submit a new one"},
+		}
+
+		writer.WriteHeader(http.StatusConflict)
+		err = statusTemplate.ExecuteTemplate(writer, "status-message", data)
+		if err != nil {
+			log.Print(err.Error())
+		}
+		return
+	}
+
+	if len(findings) > 0 {
+		data := StatusMessage{
+			"failure",
+			"Failure",
+			findings,
+		}
+
+		writer.WriteHeader(http.StatusUnprocessableEntity)
+		err = statusTemplate.ExecuteTemplate(writer, "status-message", data)
+		if err != nil {
+			log.Print(err.Error())
+		}
+		return
+	}
+
+	// Update/save week in server after validation
+	updateWeek(selected)
+
+	weekData := WeekViewData{
+		Week:     getScheduleFromMemory(),
+		EditMode: false,
+	}
+
+	statusData := StatusMessage{
+		"success",
+		"Success",
+		[]string{"Schedule has been submitted for admin review"},
+	}
+
+	viewScheduleTemplate, err := template.ParseFiles(
+		"./templates/view-schedule.html",
+		"./templates/week-view.html",
+		"./templates/status-message.html",
+	)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = viewScheduleTemplate.ExecuteTemplate(writer, "view-schedule", weekData)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
 	}
 
-	// TODO: Re-send schedule to frontend?
+	err = viewScheduleTemplate.ExecuteTemplate(writer, "status-message-oob", statusData)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
